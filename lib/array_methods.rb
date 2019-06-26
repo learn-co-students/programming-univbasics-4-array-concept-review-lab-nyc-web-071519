72193646a4c13f528aaddd3cc503af718283d9c3
tyler knapp <tgknapp11@gmail.com>
2019-06-26 17:39:31 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,5 @@
   def find_element_index(array, value_to_find)
-    array.length.times do |i|
-      if array[i] == value_to_find
-        return i
-      end
-  end
-  nil
+  return array.index(value_to_find)
 end
 
 def find_max_value(array)
