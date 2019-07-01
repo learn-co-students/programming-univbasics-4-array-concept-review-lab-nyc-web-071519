c4f552401544189f60e2804ead15382f31eb7478
Andnrew Cheon <cheonandrew113@gmail.com>
2019-07-01 23:52:48 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,16 @@
+#takes in an array and a value and returns the index of that value'
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  array.include?(value_to_find)
+    return array.index(value_to_find)
 end
 
+
+#'takes in an array of integers and returns the highest value integer'
 def find_max_value(array)
-  # Add your solution here
+  return array.max
 end
 
+#takes in an array of integers and returns the lowest value integer
 def find_min_value(array)
-  # Add your solution here
+  return array.min
 end
