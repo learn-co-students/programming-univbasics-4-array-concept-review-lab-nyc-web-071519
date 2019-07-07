f3174b9e40056a908a665da8047461032a9950f5
Alyssa Farina <alyssafarina695@gmail.com>
2019-07-07 02:55:59 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,12 @@
 def find_element_index(array, value_to_find)
-array.index(value_to_find)
+    array.include?(value_to_find)
+    return array.index(value_to_find)
 end
 
 def find_max_value(array)
- array.max
+     array.max
 end
 
 def find_min_value(array)
-array.min
+    array.min
 end
